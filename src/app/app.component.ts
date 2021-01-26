9348b7bc807487256f7b4ad39ddae20d06e2d497
tyno63 <antoninovichry@yahoo.fr>
2021-01-26 10:57:34 +0100

    mise à jour chrono

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,3 @@
-
-
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stopwatch';
+
+  public isStarted = false;
+  public isPaused = false;
+  public time = 0;
+
+  private idInterval: number|null = null;
+
+  public start(): void {
+    this.isStarted = true;
+    this.starTimer();
+  }
+
+  public stop(): void {
+    this.isStarted = false;
+    this.stopTimer();
+    this.time = 0;
+  }
+
+  public pause(): void {
+    this.isPaused = true;
+    this.stopTimer();
+  }
+
+  public continue(): void {
+    this.isPaused = false;
+    this.starTimer();
+  }
+
+  private starTimer(): void {
+    // Call stopTimer to force clear of existing interval before creating a new one.
+    this.stopTimer();
+    this.idInterval = setInterval(() => {
+      this.time++;
+    }, 1);
+  }
+
+  private stopTimer(): void {
+    if (this.idInterval !== null) {
+      clearInterval(this.idInterval);
+    }
+  }
 }
